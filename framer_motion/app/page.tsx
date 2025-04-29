3ccdfc317eb177a15c5fcf29b03356656b81559a
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-29 21:38:27 +0530

    feat: Add framer motion module starter code

---
+++ framer_motion/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full min-h-[100dvh]">
+      <div className="container mx-auto flex flex-col gap-4 p-6">
+        <h1 className="text-6xl font-bold tracking-tight">Framer Motion</h1>
+      </div>
+    </div>
+  )
+}
